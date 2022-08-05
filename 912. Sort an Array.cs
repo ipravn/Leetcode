80cdab191ac17dc266bfad9581950abbe42a21f5
Praveen Kumar <46933956+ipravn@users.noreply.github.com>
2022-08-05 17:33:29 +0530

    Create 912. Sort an Array.cs

---
+++ 912. Sort an Array.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int[] SortArray(int[] nums) {
+        int temp = 0;
+      for (int i = 0; i < nums.Length - 1; i++)
+      {
+        for (int j = i + 1; j > 0; j--)
+        {
+          if (nums[j] < nums[j -1])
+          {
+            temp = nums[j];
+            nums[j] = nums[j-1];
+            nums[j-1] = temp;
+          }
+          else
+          {
+            break;
+          }
+        }
+      }
+
+      return nums;
+    }
+}
